65a62ac4fadd93c3ca7b2b1845b358c8bb3f2e77
Alex Digrazia <alexndigrazia@hotmail.com>
2022-12-08 19:34:46 -0800

    add temporary.js file

---
+++ temporary.js
@@ -0,0 +1,9 @@
+<div class="portfolio-card" data-item="web">
+          <div class="card-body">
+            <img src="./assets/images/portfolio-2.jpg" alt="portfolio code-icon"></img>
+            <a href="#" class="card-popup-box">
+              <div>Web Development</div>
+              <h3>Skate Website</h3>
+            </a>
+          </div>
+        </div>
